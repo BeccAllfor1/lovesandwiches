c461578d398fff62b6e3c895eb41e36ca124d2be
Rebecca Allford <rebeccaallford1@gmail.com>
2024-06-06 10:12:19 +0000

    check for str

--- run.py
+++ run.py
@@ -28,11 +28,13 @@ def get_sales_data():
 
 def validate_data(values):
     """
-    inside the try, converts all string values inti integers.
+    inside the try, converts all string values into integers.
     Raises valueError if strings cannot be converted into int,
     or if there aren't exactly 6 values.
     """
+    
     try:
+        [int(value) for value in values]
         if len(values) !=6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
@@ -41,6 +43,5 @@ def validate_data(values):
         print(f" invalid data: {e}, please try again.\n")
 
 
- 
 
 get_sales_data()
